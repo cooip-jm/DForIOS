fc9131cfe7fde42d6a10297cf625e19ea92e7714
cooip-jm <78735965+cooip-jm@users.noreply.github.com>
2022-09-07 22:50:37 +0800

    Update kill-debian-dip.sh

--- kill-debian-dip.sh
+++ kill-debian-dip.sh
@@ -4,3 +4,10 @@ ip link set dev enP4p65s0 down
 ip link set dev enP4p65s0 name eth0
 ip link set eth0 up
 nmcli connection reload
+
+
+#nmcli con mod connection_name ifname eth0 type ethernet ip4 10.x.x.x/24 gw4 x.x.x.x
+#nmcli con mod connection_name ipv4.address 10.x.x.x
+#nmcli con mod connection_name ipv4.method manual
+#nmcli con mod connection_name connection.autoconnect yes
+#nmcli con up connection_name
